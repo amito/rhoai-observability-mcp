5d097fafe933d1f4447f745c0943d7eadf89574f
Amit Oren <amoren@redhat.com>
2026-02-25 10:08:14 +0200

    chore: add .dockerignore to optimize container builds
    Signed-off-by: Amit Oren <amoren@redhat.com>

--- tests/unit/test_main.py
+++ tests/unit/test_main.py
@@ -24,7 +24,6 @@ class TestMain:
     @patch("rhoai_obs_mcp.__main__.create_server")
     def test_main_reads_host_port_env(self, mock_create_server):
         """MCP_HOST and MCP_PORT env vars should configure the server."""
-        mock_server = mock_create_server.return_value
         with patch.dict(os.environ, {"MCP_HOST": "127.0.0.1", "MCP_PORT": "9090"}):
             main()
         mock_create_server.assert_called_once_with(host="127.0.0.1", port=9090)
